e06f0a8f8a253afefc78fc0b3aaa89ea27ba0d90
Armin Köck <armin.koeck@gmail.com>
2023-06-17 11:38:58 +0200

    Write code to execute the 4 different calculations with buttons

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 // variables needed for all calculations
 
 let num1 = 0;
-let num2 = 0;
+let num2 = "";
 let operator = "";
 
 
 // basic math functions
 
 function add(num1, num2) {
-    return num1 + num2
+    return +num1 + +num2
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+    return +num1 - +num2
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+    return +num1 * +num2
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+    return +num1 / +num2
 }
 
 
@@ -57,9 +57,14 @@ const numberButton = document.querySelectorAll(".numberButton");
 
 numberButton.forEach(numberButton => {
     numberButton.addEventListener("click", function(e) {
-        getNum1(e.target.value);
+        if (operator === "") {
+            getNum1(e.target.value);
+        } else if (operator !== "" && operator !== "="){
+            getNum2(e.target.value);
+        }
     });
 });
+
 
 
 // gets the value of the operator buttons
@@ -68,22 +73,57 @@ const operatorButton = document.querySelectorAll(".operatorButton");
 
 operatorButton.forEach(operatorButton => {
     operatorButton.addEventListener("click", function(e) {
-
+        if (num2 === "") {
+            operator = e.target.value
+        } else if (num2 !== "") {
+            if (e.target.value === "=") {
+                num1 = operate(num1, num2, operator);
+                num2 = "";
+                operator = "";
+            } else {
+                num1 = operate(num1, num2, operator);
+                num2 = "";
+                operator = e.target.value;
+            };
+        } else {
+            alert("Something went wrong!");
+        };
     });
 });
 
-// calculation function, that decides with if statements, if first or second number is typed in or if an operator is typed in
+/*
+1. Schritt: Alle 3 leer
+2. Schritt: Num1 voll -> Num1 erweitern oder Operator eingeben
+3. Schritt: Num1 voll, Operator voll -> Operator ändern oder Num2 eingeben
+4. Schritt: Num1 voll, Num2 voll, Operator voll -> Num2 erweitern oder 2ten Operator eingeben
+5.1 Schritt: Operator = -> Ergebnis wird in Num1 gespeichert und angezeigt, Num2 & Operator werden zurückgesetzt
+5.2 Schritt: Operator +, -, *, / -> Ergebnis wird in Num1 gespeichert und neuer Operator wird als Operator gespeichert, Num2 wird zurückgesetzt
+*/
 
+// calculation function, that decides with if statements, if first or second number is typed in or if an operator is typed in
 
 function getNum1(value) {
     num1 += value;
     changeDisplay(num1);
 }
 
+function getNum2(value) {
+    num2 += value;
+    changeDisplay(num2);
+}
 
 
+// only deletes num1, solution for num2 needed
+
+const AC = document.querySelector("#AC");
+AC.addEventListener("click", function () {
+    num1 = 0;
+    num2 = 0;
+    changeDisplay(0);
+});
 
 
+// starts with a zero on the Screen
 
 changeDisplay(0);
 
